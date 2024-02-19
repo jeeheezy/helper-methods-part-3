7410a68334a7f7a4ee79271fba4f434f68b4e7f1
Jeeho Lee <65565929+jeeheezy@users.noreply.github.com>
2024-02-19 20:37:22 +0000

    Added before_action to reduce code further

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,4 +1,5 @@
 class MoviesController < ApplicationController
+  before_action :set_movie, only: [:show, :edit, :update, :destroy]
   def new
     @movie = Movie.new
   end
@@ -14,7 +15,6 @@ class MoviesController < ApplicationController
   end
 
   def show
-    @movie = Movie.find(params.fetch(:id))
   end
 
   def create
@@ -30,12 +30,9 @@ class MoviesController < ApplicationController
   end
 
   def edit
-    @movie = Movie.find(params.fetch(:id))
   end
 
   def update
-    @movie = Movie.find(params.fetch(:id))
-    
     if @movie.update(movie_params)
       redirect_to @movie, notice: "Movie updated successfully."
     else
@@ -44,8 +41,6 @@ class MoviesController < ApplicationController
   end
 
   def destroy
-    @movie = Movie.find(params.fetch(:id))
-
     @movie.destroy
 
     redirect_to movies_url, notice: "Movie deleted successfully."
@@ -54,5 +49,10 @@ class MoviesController < ApplicationController
   private
   def movie_params
     params.require(:movie).permit(:title, :description, :image_url, :released_on)
+  end
+
+  private
+  def set_movie
+    @movie = Movie.find(params.fetch(:id))
   end
 end
